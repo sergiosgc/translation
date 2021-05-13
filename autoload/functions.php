c2474b50283b301154b80e716d21c3a67ac3c527
Sérgio Carvalho <sergiosgc@gmail.com>
2021-05-13 15:41:25 +0100

    Allow for \ namespace function overloading

--- autoload/functions.php
+++ autoload/functions.php
@@ -1,9 +1,13 @@
 <?php
+if (!function_exists('__')) {
 function __($message, ...$args) {
     $message = \sergiosgc\translation\Translation::singleton()->translate($message);
     if (class_exists('sergiosgc\sprintf', true)) $message = \sergiosgc\sprintf($message, ...$args);
     return $message;
 }
+}
+if (!function_exists('__p')) {
 function __p($message, ...$args) {
     print(__($message, ---$args));
+}
 }
